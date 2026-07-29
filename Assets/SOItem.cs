14de7fbf87f2c70a48c6412b0e3b45d3bb956715
Wanghyesung <awd5142@naver.com>
2026-07-29 16:29:29 +0900

    아이템 스크립터블 오브젝트 추가
    해당 타입(HP, Speed)에 맞게 능력 적용

--- Assets/SOItem.cs
+++ Assets/SOItem.cs
@@ -1,8 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+public enum eStatType
+{
+    HP,
+    Speed,
+    End,
+}
+
+[Serializable]
+public struct tStatValue
+{
+    public eStatType Type;
+    public float Value;
+}
+
+[CreateAssetMenu(fileName = "SOITem", menuName = "Game/Item")]
 public class SOItem : MonoBehaviour
 {
-    
+    [SerializeField] private List<tStatValue> m_listValue = new List<tStatValue>();
+    public IReadOnlyList<tStatValue> ListValue => m_listValue;
 }
